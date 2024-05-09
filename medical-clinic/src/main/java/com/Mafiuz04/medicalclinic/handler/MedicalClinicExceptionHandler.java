3d7f154541e66e35768da13bb16b7260fe67b7f4
Mafiuz04 <mathew.modrzynski@gmailc.om>
2024-05-09 22:42:58 +0200

    MED03+MED04-amendments

--- medical-clinic/src/main/java/com/Mafiuz04/medicalclinic/handler/MedicalClinicExceptionHandler.java
+++ medical-clinic/src/main/java/com/Mafiuz04/medicalclinic/handler/MedicalClinicExceptionHandler.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class MedicalClinicExceptionHandler {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgumentExceptionResponse(IllegalArgumentException ex){
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> NoSuchElementException(NoSuchElementException ex){
         return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
     @ExceptionHandler(RuntimeException.class)
